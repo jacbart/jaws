9fb054bed3b70cf273cdfe26359447ef928831b2
jacbart <jacbart@gmail.com>
2025-01-13 20:30:18 -0700

    flake lock update, added in nix forrmatter

--- flake.nix
+++ flake.nix
@@ -3,70 +3,89 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    nix-formatter-pack.url = "github:Gerschtli/nix-formatter-pack";
+    nix-formatter-pack.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self
+    , nixpkgs
+    , nix-formatter-pack
+    , ...
+    }:
     let
       inherit (nixpkgs) lib;
 
-      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      allSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       # A function that provides a system-specific Nixpkgs for the desired systems
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-
-    in {
+      forAllSystems = f:
+        allSystems (system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          });
+    in
+    {
       packages = forAllSystems ({ pkgs }:
-      let
-        utils = import ./nix/utils.nix { inherit pkgs lib self; };
-        jaws = { source, version ? (utils.mkVersion "jaws" source) }: pkgs.buildGoModule rec {
-          pname = "jaws";
-          src = source;
-          inherit version;
-          ldflags = [
-            "-s" "-w"
-            "-X 'main.Version=${version}'"
-            "-X 'main.Date=${utils.getLastModifiedDate source}'"
-          ];
-          vendorHash = null;
-          CGO_ENABLED = 1;
+        let
+          utils = import ./nix/utils.nix { inherit pkgs lib self; };
+          jaws =
+            { source
+            , version ? (utils.mkVersion "jaws" source)
+            ,
+            }:
+            pkgs.buildGoModule rec {
+              pname = "jaws";
+              src = source;
+              inherit version;
+              ldflags = [
+                "-s"
+                "-w"
+                "-X 'main.Version=${version}'"
+                "-X 'main.Date=${utils.getLastModifiedDate source}'"
+              ];
+              vendorHash = null;
+              CGO_ENABLED = 1;
 
-          meta = with pkgs.lib; {
-            mainProgram = pname;
-            description = "JAWS a cli tool for managing secrets on major cloud providers.";
-            longDescription = ''
-              JAWS was insired by AWS's bad UX for their secrets manager. The tool
-              utilizes a fuzzy finder to make filtering and selecting multiple
-              secrets easy. Once you have the secrets downloaded just edit them
-              and run the push command to update them.
-            '';
-            homepage = "https://github.com/jacbart/${pname}";
-            license = licenses.mpl20;
-            maintainers = with maintainers; [ jacbart ];
-            platforms = platforms.unix;
-          };
-        };
-      in rec {
-        ################
-        ### Packages ###
-        ################
-        bin = jaws { source = lib.cleanSource self; };
-        docker = utils.mkContainerImage "jaws" "latest" bin;
-      });
+              meta = with pkgs.lib; {
+                mainProgram = pname;
+                description = "JAWS a cli tool for managing secrets on major cloud providers.";
+                longDescription = ''
+                  JAWS was insired by AWS's bad UX for their secrets manager. The tool
+                  utilizes a fuzzy finder to make filtering and selecting multiple
+                  secrets easy. Once you have the secrets downloaded just edit them
+                  and run the push command to update them.
+                '';
+                homepage = "https://github.com/jacbart/${pname}";
+                license = licenses.mpl20;
+                maintainers = with maintainers; [ jacbart ];
+                platforms = platforms.unix;
+              };
+            };
+        in
+        rec {
+          ################
+          ### Packages ###
+          ################
+          bin = jaws { source = lib.cleanSource self; };
+          docker = utils.mkContainerImage "jaws" "latest" bin;
+        });
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
           name = "jaws";
-          buildInputs = with pkgs; [
-            figlet
-            go
-            gopls
-            gotools
-            go-tools
-            goreleaser
-            just
-            vhs
-          ] ++ lib.optional pkgs.stdenv.isLinux bitwarden-cli;
+          buildInputs = with pkgs;
+            [
+              figlet
+              go
+              gopls
+              gotools
+              go-tools
+              goreleaser
+              just
+              vhs
+            ]
+            ++ lib.optional pkgs.stdenv.isLinux bitwarden-cli;
           CGO_ENABLED = 1;
           shellHook = ''
             figlet -k "JAWS env"
@@ -74,6 +93,21 @@
         };
       });
       defaultPackage = forAllSystems ({ pkgs }: self.packages.${pkgs.stdenv.system}.bin);
+
+      # sh: nix fmt
+      formatter = allSystems (
+        system:
+        nix-formatter-pack.lib.mkFormatter {
+          pkgs = nixpkgs.legacyPackages.${system};
+          config.tools = {
+            alejandra.enable = true;
+            deadnix.enable = true;
+            nixpkgs-fmt.enable = true;
+            statix.enable = true;
+          };
+        }
+      );
+
       hydraJobs."jaws-binary" = forAllSystems ({ pkgs }: self.packages.${pkgs.stdenv.system}.bin);
     };
 }
