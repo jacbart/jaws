9856c2f0894aab4b04abd726d2d90739e48e5fe3
jacbart <jacbart@gmail.com>
2025-09-15 18:23:46 -0700

    remove old and init a new rust project

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {}
+, rustVersion
+, self
+, version
+, pname
+, ... }:
+let
+  inherit (pkgs) lib;
+  # outputHashes = { "package-version" = "sha256-xxx"; };
+  rustPlatform = pkgs.makeRustPlatform {
+    cargo = rustVersion;
+    rustc = rustVersion;
+  };
+in
+rustPlatform.buildRustPackage {
+  inherit pname version;
+  src = lib.cleanSource self;
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    # inherit outputHashes;
+  };
+  meta = with lib; {
+    description = "flake for ${pname} version ${version}";
+    homepage = "https://github.com/jacbart/";
+    license = with licenses; [ mpl20 ];
+    maintainers = with maintainers; [ jacbart ];
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {}
+, pname
+, rustVersion
+, version
+, ... }:
+
+pkgs.mkShell {
+  name = "${pname}-${version}";
+  buildInputs = with pkgs; [
+    (rustVersion.override { extensions = [ "rust-src" ]; })
+    bacon
+    rust-analyzer
+  ];
+  RUST_LOG = "debug";
+  RUST_BACKTRACE = 1;
+}
